f1eb7f8e377261911e7510be9d77d65c0b122c0b
Philipp <me@p6.gg>
2021-08-06 01:41:07 +0200

    feat(error-page): Create error page

---
+++ layouts/error.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="page-404">
+    <h1>Ahem...</h1>
+    <p>What exactly do you think you're doing?</p>
+    <p>
+      <NuxtLink :to="{ name: 'index' }"
+        >If you're looking for the Dashboard, you can go here.</NuxtLink
+      >
+    </p>
+  </div>
+</template>
